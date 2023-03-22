38fe07b1c7d4ef602b5a176530f92ca582a3795e
Pavlos Chrysochoidis <pavlos@mystenlabs.com>
2023-03-22 00:52:36 +0000

    wallet-ext: address pr comments

--- apps/wallet/src/ui/app/hooks/useGetNFTMeta.ts
+++ apps/wallet/src/ui/app/hooks/useGetNFTMeta.ts
@@ -1,11 +1,24 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { processDisplay } from '@mysten/core';
 import { is, SuiObjectData } from '@mysten/sui.js';
 import { useMemo } from 'react';
 
 import { useGetObject } from './useGetObject';
+
+export type LinkData = {
+    href: string;
+    display: string;
+};
+
+function toLinkData(link: string): LinkData | string | null {
+    try {
+        const url = new URL(link);
+        return { href: link, display: url.hostname };
+    } catch (e) {
+        return link || null;
+    }
+}
 
 export type NFTMetadata = {
     name: string | null;
@@ -14,12 +27,21 @@ export type NFTMetadata = {
 };
 
 export function useGetNFTMeta(objectID: string) {
-    const data = useGetObject(objectID, { showDisplay: true });
+    const data = useGetObject(objectID);
     const nftMeta = useMemo(() => {
         if (!data.data) return null;
         const { details } = data.data || {};
         if (!is(details, SuiObjectData) || !details.display) return null;
-        return processDisplay(details.display);
+        const { name, description, creator, img_url, link, project_url } =
+            details.display;
+        return {
+            name: name || null,
+            description: description || null,
+            imageUrl: img_url || null,
+            link: link ? toLinkData(link) : null,
+            projectUrl: project_url ? toLinkData(project_url) : null,
+            creator: creator ? toLinkData(creator) : null,
+        };
     }, [data]);
 
     return {
